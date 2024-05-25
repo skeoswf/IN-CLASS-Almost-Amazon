999aaf0036ba0aa5d2c7fd85b5b21f3288523442
Skeo <skeo051600@gmail.com>
2024-05-24 23:16:26 -0500

    complete 03L. Authors RFD

--- pages/authors.js
+++ pages/authors.js
@@ -6,6 +6,7 @@ const emptyAuthors = () => {
   renderToDOM('#store', domString);
 };
 
+// prettier-ignore
 const showAuthors = (array) => {
   clearDom();
 
@@ -21,9 +22,10 @@ const showAuthors = (array) => {
         <h5 class="card-title">${item.first_name} ${item.last_name}</h5>
         <h6 class="card-subtitle mb-2 text-muted">${item.email}</h6>
         <hr>
-        <i class="btn btn-success fas fa-eye" id="view-author-btn--${item.firebaseKey}"></i>
-        <i class="fas fa-edit btn btn-info" id="update-author--${item.firebaseKey}"></i>
-        <i class="btn btn-danger fas fa-trash-alt" id="delete-author-btn--${item.firebaseKey}"></i>
+        <p class="card-text bold">${item.favorite ? `<span class="badge badge-info sale-badge"><i class="fa fa-bell" aria-hidden="true"></i>favorite</span> ${item.ghettofix}` : ''}</p>
+        <i class="btn btn-success fas fa-eye" id="view-author-btn--${item.firebaseKey}">view</i>
+        <i class="fas fa-edit btn btn-info" id="update-author--${item.firebaseKey}">update</i>
+        <i class="btn btn-danger fas fa-trash-alt" id="delete-author-btn--${item.firebaseKey}">delete</i>
       </div>
     </div>
     `;
--- utils/startApp.js
+++ utils/startApp.js
@@ -1,3 +1,4 @@
+import { getAuthors } from '../api/authorData';
 import { getBooks } from '../api/bookData';
 import logoutButton from '../components/buttons/logoutButton';
 import domBuilder from '../components/shared/domBuilder';
@@ -5,6 +6,7 @@ import navBar from '../components/shared/navBar';
 import domEvents from '../events/domEvents';
 import formEvents from '../events/formEvents';
 import navigationEvents from '../events/navigationEvents';
+import { showAuthors } from '../pages/authors';
 import { showBooks } from '../pages/books';
 
 const startApp = () => {
@@ -17,6 +19,7 @@ const startApp = () => {
 
   // TODO: Put all books on the DOM on App load
   getBooks().then((books) => showBooks(books));
+  getAuthors.then((authors) => showAuthors(authors));
 };
 
 export default startApp;
